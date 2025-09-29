2df1934c3a238deb806cf90d486bd7af4eb8e074
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2025-09-29 14:40:47 +0200

    Add files via upload

---
+++ 001/operatory.js
@@ -0,0 +1,45 @@
+/*
+LOGICKE OPERATORY
+*/ 
+
+//logicky soucin AND &&
+console.log(5 > 3 && 5 < 3); // false
+
+//logicky soucet OR ||
+console.log(5 > 3 || 5 < 3); // true
+
+//logicka negace NOT !
+console.log(!(5 > 3)); //true
+
+//------------------------------
+
+/*
+PODMINKOVE OPERATORY
+*/ 
+
+console.log(5 == 5 ? "ano" : "ne"); // ano
+
+//------------------------------
+
+/*
+PODMINKOVE OPERATORY
+*/ 
+
+//spojeni retezcu +
+console.log("ahojda" + " ja jsem jirka");
+
+let cauko = "Jirka Král"
+console.log("ahojda ja jsem " + cauko);
+
+//alternativa
+console.log("ahojda ja jsem ${cauko}")
+
+//------------------------------
+
+/*
+POZZOR
+*/
+
+console.log(5 + "3" );
+console.log(5 - "3");
+
