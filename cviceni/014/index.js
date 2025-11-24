dc46e5928887b5fcc442f788b479c8cfcc284338
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2025-11-24 14:44:54 +0100

    Add files via upload

---
+++ cviceni/014/index.js
@@ -0,0 +1,11 @@
+function zmenitNadpis(){
+    document.getElementById("nadpis").innerHTML = "text byl zmenen";
+}
+
+function textmodry(){
+    document.getElementById("paragraf").style.color = "blue";
+}
+
+function textpuvodni(){
+    document.getElementById("paragraf").style.color = "black";
+}
