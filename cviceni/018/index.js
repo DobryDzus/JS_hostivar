c2932fc500e34d64497b4c504629813a158859bc
DobryDzus <77481178+DobryDzus@users.noreply.github.com>
2025-12-08 10:45:22 +0100

    Add files via upload

---
+++ cviceni/018/index.js
@@ -0,0 +1,11 @@
+document.getElementById("nadpis").innerHTML = "Novy text 1";
+
+document.getElementById("odstavec").innerHTML = "druhy novy text";
+
+function zmenBarvu(){
+    document.getElementById("tlacitko").style.backgroundColor = "#ff00ff";
+}
+
+document.getElementById("div").style.backgroundColor = "green";
+document.getElementById("div").style.width = "200px";
+document.getElementById("div").style.height = "100px";
